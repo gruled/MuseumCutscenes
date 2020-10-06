b58ef23d27d0b093b48e8df0ae5cae3a6186b8fc
Dmitriy Chukarin <fysiks@yandex.ru>
2020-10-06 13:01:48 +0400

    Add Smth else

--- Assets/New/Scripts/InnerObjectsLife.cs
+++ Assets/New/Scripts/InnerObjectsLife.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class InnerObjectsLife : MonoBehaviour
 {
--- Assets/New/Scripts/OuterZoom.cs
+++ Assets/New/Scripts/OuterZoom.cs
@@ -11,9 +11,11 @@ public class OuterZoom : MonoBehaviour
     [SerializeField] private float FOVMax;
     [SerializeField] private float FOVMin;
     private float _prevDist;
+    private float _dist;
+    private float _elderDist;
 
     private float _speedModifier;
-    
+
     void OnEnable()
     {
         _camera = transform.GetComponent<Camera>();
@@ -24,9 +26,28 @@ public class OuterZoom : MonoBehaviour
     private IEnumerator _increaseSpeedZoom()
     {
         Sequence sequence = DOTween.Sequence();
-        sequence.Insert(0.0f,  DOTween.To(()=> _speedModifier, x => _speedModifier = x, 1.0f, 0.5f) );
+        sequence.Insert(0.0f, DOTween.To(() => _speedModifier, x => _speedModifier = x, 1.0f, 0.5f));
         sequence.Play();
-        yield return  new WaitWhile(sequence.IsPlaying);
+        yield return new WaitWhile(sequence.IsPlaying);
+    }
+
+    private IEnumerator _enertion()
+    {
+        Sequence sequence = DOTween.Sequence();
+        float endValue = _camera.fieldOfView - (_prevDist - _elderDist) * 0.3f;
+        if (endValue > FOVMax)
+        {
+            endValue = FOVMax;
+        }
+
+        if (endValue < FOVMin)
+        {
+            endValue = FOVMin;
+        }
+
+        sequence.Insert(0.0f, _camera.DOFieldOfView(endValue, 1.0f));
+        sequence.Play();
+        yield return new WaitWhile(sequence.IsPlaying);
     }
 
     private IEnumerator _zoom()
@@ -45,21 +66,31 @@ public class OuterZoom : MonoBehaviour
                 }
                 else if (touch0.phase == TouchPhase.Moved || touch1.phase == TouchPhase.Moved)
                 {
-                    float dist = Vector2.Distance(touch0.position, touch1.position);
-                    if (dist < _prevDist)
+                    _dist = Vector2.Distance(touch0.position, touch1.position);
+                    float d = Math.Abs(_dist - _prevDist);
+                    if (_dist < _prevDist)
                     {
-                        if (_camera.fieldOfView < FOVMax)
+                        _camera.fieldOfView += _speed * _speedModifier*d;
+                        if (_camera.fieldOfView > FOVMax)
                         {
-                            _camera.fieldOfView += _speed * _speedModifier;
+                            _camera.fieldOfView = FOVMax;
                         }
                     }
                     else
                     {
-                        if (_camera.fieldOfView > FOVMin)
+                        _camera.fieldOfView -= _speed * _speedModifier*d;
+                        if (_camera.fieldOfView < FOVMin)
                         {
-                            _camera.fieldOfView -= _speed * _speedModifier;
+                            _camera.fieldOfView = FOVMin;
                         }
                     }
+
+                    _elderDist = _prevDist;
+                    _prevDist = _dist;
+                }
+                if (touch0.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Ended)
+                {
+                    StartCoroutine(_enertion());
                 }
             }
 
